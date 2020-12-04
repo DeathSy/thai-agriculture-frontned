1fa470c16c5a919f1ae03008b9cc7311ffa18624
Ajarnnotes <ajarnnotes@gmail.com>
2020-12-04 18:06:31 +0700

    ADD routes

---
+++ src/config/routes.js
@@ -0,0 +1,27 @@
+import { lazy } from "react";
+
+export default{
+  Home:{
+    path: "/",
+    exact: true,
+    component: lazy(()=> import('../pages/Home'))
+  },
+
+  Welcome:{
+    path: "/",
+    exact: true,
+    component: lazy(()=> import('../pages/Welcome'))
+  },
+
+  Login:{
+    path: "/Login",
+    exact: true,
+    component: lazy(()=> import('../pages/Login'))
+  },
+
+  Register:{
+    path: "/Register",
+    exact: true,
+    component: lazy(()=> import('../pages/Register'))
+  }
+}
