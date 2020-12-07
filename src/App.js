58d9ca5bde1cb928f92564e944e1b76eb5a3f7b0
walax31 <walax31009@gmail.com>
2020-12-07 15:26:43 +0700

    ADD home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import GlobalStyle from './components/GlobalStyle'
+import Home from './pages/Home'
 
 function App () {
   return (
-    <div className="App">
-     <GlobalStyle/>
-    </div>
+    <>
+     <GlobalStyle />
+      <Home/>
+    </>
   )
 }
 
