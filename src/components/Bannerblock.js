ec81b3af79371ce60f71c382baea1bd07acdf6d1
walax31 <walax31009@gmail.com>
2020-12-07 17:57:09 +0700

    EDIT css background

--- src/components/Bannerblock.js
+++ src/components/Bannerblock.js
@@ -21,7 +21,7 @@ const BackgroundOverlay = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0,0,0,0.4);
+  background-color: rgba(0,0,0,0.2);
 `
 
 function BannerBlock ({ src, height = 100, children }) {
