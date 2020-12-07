ef3238fd545e7f372c2c0302dc4c3ce3f91e1cde
Ajarnnotes <ajarnnotes@gmail.com>
2020-12-07 16:47:18 +0700

    EDIT navebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import LogoBlock from '../components/LogoBlock'
 import logoImage from '../assets/logo.png'
 
 export const Action = styled.div`
@@ -24,7 +23,7 @@ align-items: center;
 width: 100%;
 position: fixed;
 background-color: #ffffff;
-z-index:1000
+z-index:1000;
 `
 export const ActionContainer = styled.div`
 display: flex;
@@ -37,17 +36,17 @@ margin:0 2rem;
 function Navbar () {
   return (
     <>
-    <Wrapper>
-      <LogoBlock src={logoImage}/>
-      <ActionContainer>
-        <Action>
-          <>Login</>
-        </Action>
-        <Action>
-          <>Register</>
-        </Action>
-      </ActionContainer>
-    </Wrapper>
+      <Wrapper>
+        <Logo src={logoImage} />
+        <ActionContainer>
+          <Action>
+            <>Login</>
+          </Action>
+          <Action>
+            <>Register</>
+          </Action>
+        </ActionContainer>
+      </Wrapper>
     </>
   )
 }
