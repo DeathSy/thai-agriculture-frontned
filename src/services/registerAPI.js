2cd24d8ba52ddcd2fc40c834d638ed84dbdf5dbf
Ajarnnotes <ajarnnotes@gmail.com>
2020-12-07 15:16:38 +0700

    EDIT register and fetcgData serveices

--- src/services/registerAPI.js
+++ src/services/registerAPI.js
@@ -1,19 +1,20 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-export default function RegisterAPI(values) {
-  console.log(values)
-    axios ( {
-      url: 'http://localhost:3333/register',
-      method: 'post',
-      data: {
-        "username": values.username,
-        "email": values.email,
-        "phone_number": values.phone_number,
-        "password": values.password
-      },
-      responseType: 'json', 
-    }) .then(response => {
-      console.log(response) 
+// export default function RegisterAPI(values) {
+//   console.log(values)
+//     axios ( {
+//       url: 'http://localhost:3333/register',
+//       method: 'post',
+//       data: {
+//         "username": values.username,
+//         "email": values.email,
+//         "phone_number": values.phone_number,
+//         "password": values.password
+//       },
+//       responseType: 'json',
+//     }) .then(response => {
+//       console.log(response)
 
-    })
-}
+//     })
+
+// }
