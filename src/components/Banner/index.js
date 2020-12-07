2e0f16ede9deb5e006a62e8887a50d74c05f7910
walax31 <walax31009@gmail.com>
2020-12-07 16:11:44 +0700

    ADD image in banner component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,28 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import BackgroundBlock from './BackgroundBlock'
+import Img from '../../assets/banner3.jpg'
 
 const Container = styled.div`
   width: 100vw;
-  height: 20vh;
+  height: 40vh;
   background-color: red;
 `
 const Bannerimg = styled.div`
   width: 100vh;
-  height: 20vh;
+  height: auto;
   background-color: red;
 `
 
-const Img = styled.div`
-  width: 100vh;
-  height: 20vh;
-  background-color: red;
-`
 function Banner () {
   return (
     <div>
       <Container>
         <Bannerimg>
-          <Img />
+          <BackgroundBlock src={Img} />
         </Bannerimg>
       </Container>
     </div>
