a78cdbbeb76f55edf2b3c384e2e336aa09e086ee
devillkaow <devillkaow@gmail.com>
2020-12-07 16:30:18 +0700

    EDIT App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import routes from './config/routes'
 import Authprovider from './contexts/ActionContext'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import GlobalStyle from './components/GlobalStyle'
 
 function App () {
