a5d219908f8644b0a8b4b1ab116cd2f12ce7b324
Ajarnnotes <ajarnnotes@gmail.com>
2020-12-07 15:31:41 +0700

    ADD navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import styled from 'styled-components'
+import logoImage from '../assets/logo.png'
+
+export const ActionRegister = styled.div`
+padding:.7rem 4rem;
+margin:0 .5rem;
+background-color: #10c18b;
+border-radius: 60px;
+:hover {
+  background-color: #ffffff ;
+  border-radius: 60px;
+};
+`
+export const ActionLogin = styled.div`
+padding:.7rem 4rem;
+margin:0 .5rem;
+:hover {
+  background-color: #00a472;
+  border-radius: 60px;
+};
+`
+export const Logo = styled.div`
+padding:40px;
+background-color: #e0e0e0;
+margin:.5rem;
+`
+
+export const Wrapper = styled.div`
+display: flex;
+flex: 1;
+align-items: center;
+width: 100%;
+position: fixed;
+background-color: #ffffff;
+`
+export const ActionContainer = styled.div`
+display: flex;
+flex: 1;
+justify-content: flex-end;
+align-items: center;
+margin:0 2rem;
+`
+
+function Navbar () {
+  return (
+    <>
+    <Wrapper>
+      <Logo src={logoImage}/>
+      <ActionContainer>
+        <ActionLogin>
+          <>Login</>
+        </ActionLogin>
+        <ActionRegister>
+          <>Register</>
+        </ActionRegister>
+      </ActionContainer>
+    </Wrapper>
+    </>
+  )
+}
+
+export default Navbar
