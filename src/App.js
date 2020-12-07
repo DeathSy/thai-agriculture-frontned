ced3baa87ffbf50f4cfc4595f9c53d6f57629901
walax31 <walax31009@gmail.com>
2020-12-07 15:34:58 +0700

    EDIT app

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import Home from './pages/Home'
 function App () {
   return (
     <>
-<<<<<<< HEAD
      <GlobalStyle />
       <Home/>
-=======
       <Navbar />
-      <GlobalStyle />
->>>>>>> 01c27385f6176a6d72fe96d56acb2fe8056342cf
+      
     </>
   )
 }
