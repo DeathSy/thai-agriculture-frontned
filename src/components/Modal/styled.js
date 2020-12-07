60423e7acc9ca2a0c199138ba45acf2360228018
ramita-kahapana <ramitakahapana@gmail.com>
2020-12-07 18:04:40 +0700

    ADD styled.js Modal

---
+++ src/components/Modal/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+
+
+// Start-Content
+export const ContentDetail = styled.div`
+ 
+`;
+export const TextTitle = styled.h2`
+ 
+`;
+export const InputDevice = styled.input`
+  width: 50%;
+  margin: 10px 0;
+  padding: 5px 10px;
+  height: 20px;
+  border: solid 3px black;
+  outline: none;
+  &:focus {
+    border: 1px solid #0c1737;
+  }
+`;
+// End-Content
+
+// Start-Footer
+export const FooterDevice = styled.div`
+`;
+export const ButtonAdd = styled.button`
+
+`;
+// End-Footer
