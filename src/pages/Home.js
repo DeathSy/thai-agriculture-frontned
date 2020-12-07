dd849877096b109c5f6874a5b170140af1f56d68
walax31 <walax31009@gmail.com>
2020-12-07 10:13:56 +0700

    ADD draft home page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+import withHelmet from '../utils/withHelmet'
+
+
+function Home() {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default withHelmet('Agogreen | Home')(withNavbar(Home))
