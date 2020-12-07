a390626c2a53b69682b35ed190da1dd2ae08dbb2
Ajarnnotes <ajarnnotes@gmail.com>
2020-12-07 15:14:38 +0700

    ADD banner block

---
+++ src/components/Bannerblock.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const BackgroundImage = styled.div`
+  height: 100vh;
+  width: auto;
+  background-image: url(${props => props.src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-y: -20px;
+
+  position: relative;
+`
+
+const BackgroundOverlay = styled.div`
+  position: absolute;
+  width: 100%; 
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0,0,0,0.4);
+`
+
+function BannerBlock ({ src, height = 100, children }) {
+  return (
+    <BackgroundImage src={src} height={height}>
+      <BackgroundOverlay>
+        {children}
+      </BackgroundOverlay>
+    </BackgroundImage>
+  )
+}
+
+export default BannerBlock
