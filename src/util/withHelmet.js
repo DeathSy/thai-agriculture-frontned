f3e6e3c6a9e7af7afadf69209ca7d0e016d5bc88
DogeCnx <Saratoga1412@gmail.com>
2020-12-08 18:13:51 +0700

    EDIT ccode format

--- src/util/withHelmet.js
+++ src/util/withHelmet.js
@@ -5,7 +5,10 @@ function withHelmet (title) {
   return Component => props => (
     <>
       <Helmet>
-        <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap' rel='stylesheet' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap'
+          rel='stylesheet'
+        />
         <title>{title}</title>
       </Helmet>
       <Component {...props} />
