1609255a42c303bfe5c688062937b2dd7021a798
DogeCnx <Saratoga1412@gmail.com>
2020-12-08 18:38:40 +0700

    EDIT merge pull request

--- src/components/Modal/ModalInfomation.js
+++ src/components/Modal/ModalInfomation.js
@@ -1,22 +1,38 @@
 import React from 'react'
-import { ContentDetail, ContentModal, TextTitle, Title, InputDevice, ButtonAdd, ButtonClose, HeaderDevice, FooterDevice } from './styled'
+import {
+  ContentDetail,
+  ContentSelect,
+  TextTitle,
+  ContentMinMax,
+  InputDevice,
+  ButtonAdd,
+  FooterDevice
+  // customStyles
+} from './styled'
 import Select from 'react-select'
+
 const options = [
   { value: 'Temperature', label: 'Temperature' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
+  { value: 'Humidity', label: 'Humidity' },
+  { value: 'PM 1', label: 'PM 1' },
+  { value: 'PM 2.5', label: 'PM 2.5' },
+  { value: 'PM 10', label: 'PM 10' },
+  { value: 'Wind Direction', label: 'Wind Direction' },
+  { value: 'Wind Velocity', label: 'Wind Velocity' },
+  { value: 'Corrosion', label: 'Corrosion' },
+  { value: 'Pressure', label: 'Pressure' }
 ]
 function ModalAddDevice () {
   return (
     <>
       <ContentDetail>
         <ContentSelect>
-        <TextTitle>Device ID</TextTitle>
-        <Select options={options} />
+          <TextTitle>Device ID</TextTitle>
+          <Select styles={ContentSelect.select} options={options} />
         </ContentSelect>
         <ContentMinMax>
-        <TextTitle>Device Name</TextTitle>
-        <InputDevice value={valueName} onChange={handleInputName} />
+          <TextTitle>Device Name</TextTitle>
+          <InputDevice type='number' />
         </ContentMinMax>
       </ContentDetail>
       <FooterDevice>
