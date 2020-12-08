126ee98269d4e6c6c92d422f1bbc275e8301938e
walaluck khamnoi <walax31009@gmail.com>
2020-12-08 17:51:39 +0700

    EDIT format

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,19 +5,17 @@ import Banner from '../components/Banner/index'
 import Content from '../components/Content/index'
 
 const Container = styled.div`
-  width:100vw;
+  width: 100vw;
   height: 200vh;
-  background-color:firebrick;
+  background-color: firebrick;
 `
 
 function Home () {
   return (
-    <div>
-      <Container>
-        <Banner />
-        <Content />
-      </Container>
-    </div>
+    <Container>
+      <Banner />
+      <Content />
+    </Container>
   )
 }
 
