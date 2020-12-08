aeded10d4861fc10e369e0343ab05f0f93274a66
DogeCnx <Saratoga1412@gmail.com>
2020-12-08 17:26:03 +0700

    ADD userAPI

---
+++ src/services/userAPI.js
@@ -0,0 +1,32 @@
+//create user 
+export async function createDevice(value) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(value)
+      //"username": values.username,
+      //"email": values.email,
+      //"phone_number": values.phone_number,
+      //"password": values.password
+  }
+  return fetch(`http://0.0.0.0:3333/api/v1/_null_/`, requestOptions)
+    .then(response => response.json())
+}
+
+//login
+export async function createDevice(tokenUser,value) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(value)
+      //"username": values.username,
+      //"password": values.password
+  }
+  return fetch(`http://0.0.0.0:3333/api/v1/_null_/`, requestOptions)
+    .then(response => response.json())
+}
+//logout
