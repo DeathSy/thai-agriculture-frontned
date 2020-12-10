1ed3a91e0a9635386811937a91cf1000a057e591
Nopparat Kaewsai <devillkaow@gmail.com>
2020-12-10 10:48:40 +0700

    EDIT Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Action, Wrapper, ActionContainer } from './styled'
+import Logo from './Logo'
+import Imglogo from '../../assets/logo.png'
+import useScroll from '../../util/useScroll'
+import { Link } from 'react-router-dom'
+import ButtonAddDevice from '../ButtonModal/ButtonDevice'
+
+function Navbar () {
+  const isScroll = useScroll({ scrollRange: 20 })
+  const i = 0
+  if (i === 1) {
+    return (
+      <Wrapper isScroll={isScroll}>
+        <Logo src={Imglogo} />
+        <ActionContainer>
+          <Action>
+            <Link to='/Login'>Login</Link>
+          </Action>
+          <Action>
+            <Link to='/Register'>Register</Link>
+          </Action>
+        </ActionContainer>
+      </Wrapper>
+    )
+  } else {
+    return (
+      <Wrapper isScroll={isScroll}>
+        <Logo src={Imglogo} />
+        <ActionContainer>
+          <ButtonAddDevice />
+          <Action>Logout</Action>
+        </ActionContainer>
+      </Wrapper>
+    )
+  }
+}
+
+export default Navbar
