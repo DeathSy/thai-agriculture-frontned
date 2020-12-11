6e59326012d277e871ef798f04881a952c0d17e2
Ajarnshilin <ajarnnotes@gmail.com>
2020-12-11 16:39:55 +0700

    EDIT code format

--- src/services/userAPI.js
+++ src/services/userAPI.js
@@ -1,32 +1,32 @@
-//create user 
-export async function register(value) {
+// create user
+export async function register (value) {
   const requestOptions = {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(value)
-      //"username": values.username,
-      //"email": values.email,
-      //"phone_number": values.phone_number,
-      //"password": values.password
+    // "username": values.username,
+    // "email": values.email,
+    // "phone_number": values.phone_number,
+    // "password": values.password
   }
-  return fetch(`http://0.0.0.0:3333/api/v1/_null_/`, requestOptions)
+  return fetch('http://0.0.0.0:3333/api/v1/_null_/', requestOptions)
     .then(response => response.json())
 }
 
-//login
-export async function login(value) {
+// login
+export async function login (value) {
   const requestOptions = {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(value)
-      //"username": values.username,
-      //"password": values.password
+    // "username": values.username,
+    // "password": values.password
   }
-  return fetch(`http://0.0.0.0:3333/api/v1/_null_/`, requestOptions)
+  return fetch('http://0.0.0.0:3333/api/v1/_null_/', requestOptions)
     .then(response => response.json())
 }
-//logout
+// logout
