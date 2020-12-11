9c4a4ffb4fc7f95d1ae7b56e1f3188c6d3762afb
Ramita <ramitakahapana@gmail.com>
2020-12-11 11:53:35 +0700

    EDIT from infomation modal

--- src/components/Modal/styled.js
+++ src/components/Modal/styled.js
@@ -5,17 +5,18 @@ export const ContentDetail = styled.div`
   padding: 10px 0px;
 `
 export const TextTitle = styled.h2`
-font-size:1.5rem;
-margin-top:10px;`
+  font-size: 1.5rem;
+  margin-top: 10px;
+`
 export const InputDevice = styled.input`
   width: 93%;
   height: 25px;
   margin: 10px 0;
   padding: 5px 10px;
-  border-radius:20px;
+  border-radius: 20px;
   border: solid 2px black;
   outline: none;
-  font-size:20px;
+  font-size: 20px;
   &:focus {
     border: 1px solid #0c1737;
   }
@@ -23,9 +24,22 @@ export const InputDevice = styled.input`
 export const ContentMinMax = styled.div``
 export const ContentSelect = styled.div``
 export const Select = styled.select``
-export const InputInfomation = styled.input`
-  
-`
+export const InputInfomation = styled.input``
+export const customStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: '1px solid #000',
+    color: state.isSelected ? 'red' : 'blue',
+    padding: 25,
+    fontSize: '20px'
+  }),
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.0 : 1
+    const transition = 'opacity 300ms'
+
+    return { ...provided, opacity, transition }
+  }
+}
 
 // End-Content
 
