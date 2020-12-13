6210b5805afa8b3dfcce1c7b52c88af46c3530e8
DogeCnx <thiraphat.re0096@gmail.com>
2020-12-13 16:24:37 +0700

    ADD  updateDeviceSetting function

--- src/services/newInfo.js
+++ src/services/newInfo.js
@@ -23,3 +23,28 @@ export async function createDeviceSetting (tokenUser, value) {
   return fetch(`${process.env.REACT_APP_API_ENDPOINT}/v1/_null_`, requestOptions)
     .then(response => response.json())
 }
+// update setting default data device
+export async function updateDeviceSetting (tokenUser, value) {
+  const requestOptions = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${tokenUser}`
+    },
+    body: JSON.stringify(value)
+    // id_user : value.id_user,
+    // id_device : value.id_device,
+    // All max,min
+    // "temperature": values.temperature,
+    // "humidity": values.humidity,
+    // "pm1": values.pm1,
+    // "pm25": values.pm25,
+    // "pm10": values.pm10,
+    // "wind_direction": values.wind_direction,
+    // "wind_velocity": values.wind_velocity,
+    // "colosion": values.colosion,
+    // "pressure": values.pressure
+  }
+  return fetch(`${process.env.REACT_APP_API_ENDPOINT}/v1/_null_`, requestOptions)
+    .then(response => response.json())
+}
