995eab0bd8066f90458b9fea587872883061c1cd
DogeCnx <thiraphat.re0096@gmail.com>
2020-12-15 00:54:14 +0700

    ADD Provider in index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { UserContextProvider } from './contexts/UserContext'
+import { ActionContextProvider } from './contexts/ActionContext'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ActionContextProvider>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </ActionContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
