f89460cb93be3a21df538575be9ecb978a3817ff
DogeCnx <thiraphat.re0096@gmail.com>
2020-12-15 01:08:51 +0700

    EDIT route connect and delete function fatchDataById

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,15 +1,3 @@
-// fetch get data by Id device
-export async function fetchDataById (tokenUser, idDevice) {
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${tokenUser}`
-    }
-  }
-  return fetch(`https://worawanbydiistudent.store/api/v1/${idDevice}/t/latest`, requestOptions)
-    .then(response => response.json())
-}
 // fetch get DeviceData by id user
 export async function fetchDeviceById (tokenUser, idUser) {
   const requestOptions = {
@@ -20,6 +8,6 @@ export async function fetchDeviceById (tokenUser, idUser) {
     },
     body: JSON.stringify({ idUser: idUser })
   }
-  return fetch(`${process.env.REACT_APP_API_ENDPOINT}/v1/_null_/${idUser}`, requestOptions)
+  return fetch(`${process.env.REACT_APP_API_ENDPOINT}/v1/check_user_device`, requestOptions)
     .then(response => response.json())
 }
