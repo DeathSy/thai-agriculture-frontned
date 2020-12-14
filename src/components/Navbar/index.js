3eb32903be4a03c434874050d4b9d46cf19d56d3
Nopparat Kaewsai <devillkaow@gmail.com>
2020-12-14 17:04:08 +0700

    EDIT Navbar delete useScroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,15 +2,13 @@ import React from 'react'
 import { Action, Wrapper, ActionContainer } from './styled'
 import Logo from './Logo'
 import Imglogo from '../../assets/field.png'
-import useScroll from '../../util/useScroll'
 import { Link } from 'react-router-dom'
 
 function Navbar () {
-  const isScroll = useScroll({ scrollRange: 20 })
   const i = 0
   if (i === 1) {
     return (
-      <Wrapper isScroll={isScroll}>
+      <Wrapper>
         <Logo src={Imglogo} />
         <ActionContainer>
           <Action>
@@ -24,7 +22,7 @@ function Navbar () {
     )
   } else {
     return (
-      <Wrapper isScroll={isScroll}>
+      <Wrapper>
         <Logo src={Imglogo} />
         <ActionContainer>
           <Action>Logout</Action>
